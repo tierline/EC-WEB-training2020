f06f5084223e328d0d0127bfb5e6576ad52d42ff
tierkato <kato@tierline.com>
2020-12-07 11:36:44 +0900

    MEMBERS の email カラムに Unique key を設定

--- db/createTable.sql
+++ db/createTable.sql
@@ -13,7 +13,8 @@ create table MEMBERS (
 id int auto_increment primary key,
 email varchar(128),
 password varchar(256),
-address varchar(256)
+address varchar(256),
+UNIQUE(email)
 );
 
 create table CATEGORY(
