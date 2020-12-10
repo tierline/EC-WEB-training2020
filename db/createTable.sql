7bcf5b75599b34b48c2e2b68d8e59740b8f77f55
tierkato <kato@tierline.com>
2020-12-10 09:59:41 +0900

    SQL文の表記を修正

--- db/createTable.sql
+++ db/createTable.sql
@@ -4,55 +4,57 @@ create database training;
 
 use training;
 
-create table ADMINS (
-id bigint auto_increment,
-name varchar(128),
-password varchar(256),
+CREATE TABLE admins (
+id INT AUTO_INCREMENT,
+name VARCHAR(128),
+password VARCHAR(256),
 PRIMARY KEY (id),
 UNIQUE(name)
  );
 
-create table MEMBERS (
-id bigint auto_increment,
-email varchar(128),
-password varchar(256),
-address varchar(256),
+CREATE TABLE members (
+id INT AUTO_INCREMENT,
+email VARCHAR(128),
+password VARCHAR(256),
+address VARCHAR(256),
 PRIMARY KEY (id),
 UNIQUE(email)
 );
 
-create table CATEGORY(
-id int auto_increment primary key,
-name varchar(16));
-
-create table product(
-id int auto_increment primary key,
-category_id int,
-name varchar(128),
-price int,
-image_path varchar(20),
-description text
+CREATE TABLE category(
+id INT AUTO_INCREMENT,
+name VARCHAR(16),
+PRIMARY KEY (id)
 );
 
+CREATE TABLE product(
+id INT AUTO_INCREMENT,
+category_id INT,
+name VARCHAR(128),
+price INT,
+image_path VARCHAR(20),
+description TEXT,
+PRIMARY KEY (id)
+);
 
-CREATE TABLE `ORDER_ITEMS` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) DEFAULT NULL,
-  `name` varchar(128) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
-  `image_path` varchar(20) DEFAULT NULL,
-  `description` text,
-  `quantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE order_items (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  order_id INT(11) DEFAULT NULL,
+  name VARCHAR(128) DEFAULT NULL,
+  price INT(11) DEFAULT NULL,
+  image_path VARCHAR(20) DEFAULT NULL,
+  description TEXT,
+  quantity INT(11) DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- training.orders definition
-CREATE TABLE `ORDERS` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `phone` varchar(12) DEFAULT NULL,
-  `name` varchar(12) DEFAULT NULL,
-  `address` text,
-  `email` text,
-  `price` int(12),
-  `date` varchar(48) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+
+CREATE TABLE orders (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  phone VARCHAR(12) DEFAULT NULL,
+  name VARCHAR(12) DEFAULT NULL,
+  address TEXT,
+  email TEXT,
+  price INT(12),
+  date VARCHAR(48) DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
