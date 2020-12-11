49b6e954b62e25605231fc1460c1f2d801598360
tierkato <kato@tierline.com>
2020-12-11 15:50:46 +0900

    トップ画面に商品を表示

--- db/insert_product.sql
+++ db/insert_product.sql
@@ -1,17 +1,16 @@
 use training;
 
--- load data  local infile "./test_data.csv"
--- into table PRODUCT
--- fields terminated by ',' enclosed by'"'
--- lines terminated by '\r\n'
--- ignore 1 lines;
+load data  local infile "./test_data.csv"
+into table PRODUCT
+fields terminated by ',' enclosed by'"'
+lines terminated by '\r\n'
+ignore 1 lines;
 
-
--- load data  local infile "./category.csv"
--- into table CATEGORY
--- fields terminated by ',' enclosed by'"'
--- lines terminated by '\r\n'
--- ignore 1 lines;
+load data  local infile "./category.csv"
+into table CATEGORY
+fields terminated by ',' enclosed by'"'
+lines terminated by '\r\n'
+ignore 1 lines;
 
 insert into MEMBERS(email, password, address, lastUpdatedBy, status)
 values("test@example.com", "$2a$10$uhuqnvtjTayBhSjs7ezeB.2DG5GlIERAawzRoCROyTxWpzwKy7T.e", "hyogo", "none", "approval");
--- src/main/java/com/example/training/common/repository/OrderRepository.java
+++ src/main/java/com/example/training/common/repository/OrderRepository.java
@@ -2,18 +2,18 @@ package com.example.training.common.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
 import com.example.training.common.domain.Order;
 import com.example.training.common.domain.OrderItem;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface OrderRepository {
 
 	public int save(Order order);
 
-	public void saveItem(@Param("item") OrderItem item, int id);
+	public void saveItem(@Param("item") OrderItem item, @Param("id") int id);
 
 	public List<OrderItem> findItemsByOrder(Order order);
 
