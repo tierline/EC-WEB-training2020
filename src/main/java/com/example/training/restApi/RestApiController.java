70814bca0433d332392e4144b02f6b60274cd6d1
tierkato <kato@tierline.com>
2020-12-15 12:29:15 +0900

    tweak

--- src/main/java/com/example/training/restApi/RestApiController.java
+++ src/main/java/com/example/training/restApi/RestApiController.java
@@ -2,12 +2,16 @@ package com.example.training.restApi;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import com.example.training.common.domain.Cart;
 import com.example.training.common.domain.Product;
 import com.example.training.common.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
+
+	@Autowired
+	private HttpSession session;
+
 	@Autowired
 	private ProductRepository productRepository;
 
@@ -30,8 +38,25 @@ public class RestApiController {
 		return items;
 	}
 
-	@PostMapping("/post")
-	public String post() {
-		return "posted";
+	/**
+	 * @return カートに商品の追加
+	 */
+	@PostMapping("/cart/add/{id}")
+	public String add(@PathVariable int id) {
+		Cart cart = (Cart) session.getAttribute(Cart.SESSION_NAME);
+		Product product = productRepository.findId(id).orElseThrow();
+		cart.add(product);
+
+		session.setAttribute(Cart.SESSION_NAME, cart);
+		return "id番号" + id + "の商品" + product + "をカートに保存しました";
+	}
+
+	/**
+	 * @return セッション情報を見る
+	 */
+	@GetMapping("/cart/show")
+	public Object show() {
+		Cart cart = (Cart) session.getAttribute(Cart.SESSION_NAME);
+		return cart;
 	}
 }
