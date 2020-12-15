021d996c2c9cf9501f6d755ce49df9a57cfb63b3
tsukamoto <tsukamoto@tierline.com>
2020-12-15 14:37:05 +0900

    API追加

--- src/main/java/com/example/training/restApi/RestApiController.java
+++ src/main/java/com/example/training/restApi/RestApiController.java
@@ -1,15 +1,23 @@
 package com.example.training.restApi;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.training.common.domain.Product;
 import com.example.training.common.repository.ProductRepository;
+import com.example.training.member.Service.MemberService;
+import com.example.training.member.domain.Member;
+import com.example.training.member.repository.MemberRepository;
 
 @CrossOrigin
 @RestController
@@ -18,14 +26,35 @@ public class RestApiController {
 	@Autowired
 	private ProductRepository productRepository;
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello";
-	}
+	@Autowired
+	private MemberService memberService;
 
-	@GetMapping("/products")
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@GetMapping("/common/products")
 	public List<Product> product() {
 		List<Product> items = productRepository.findAll();
 		return items;
+	}
+
+	@PostMapping("/members/auth/applicate")
+	@ResponseBody
+	public Member create(@RequestBody Member member) {
+		memberService.create(member);
+		return member;
+	}
+
+	@PostMapping("/members/auth/login")
+	@ResponseBody
+	public Boolean login(@RequestBody Member member) {
+		Optional<Member> memberDetail = memberRepository.findByEmail(member.getEmail());
+		if (memberDetail.isPresent()) {
+			return true;
+		}
+		return false;
 	}
 }
