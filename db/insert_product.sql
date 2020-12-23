424673f4b0a16265a8e542f767dcb635b0fc06f9
tierkato <kato@tierline.com>
2020-12-23 10:11:29 +0900

    テーブル名を小文字に変更

--- db/insert_product.sql
+++ db/insert_product.sql
@@ -1,22 +1,22 @@
 use training;
 
 load data  local infile "./test_data.csv"
-into table PRODUCT
+into table product
 fields terminated by ',' enclosed by'"'
 lines terminated by '\r\n'
 ignore 1 lines;
 
 load data  local infile "./category.csv"
-into table CATEGORY
+into table category
 fields terminated by ',' enclosed by'"'
 lines terminated by '\r\n'
 ignore 1 lines;
 
-insert into MEMBERS(email, password, address, lastUpdatedBy, status)
+insert into members(email, password, address, lastUpdatedBy, status)
 values("test@example.com", "$2a$10$uhuqnvtjTayBhSjs7ezeB.2DG5GlIERAawzRoCROyTxWpzwKy7T.e", "hyogo", "none", "approval");
 
-insert into MEMBERS(email, password, address, lastUpdatedBy, status)
+insert into members(email, password, address, lastUpdatedBy, status)
 values("test2@example.com", "$2a$10$EQyVZTFajqzPEHVtmFXW2O7tq.czJbKO/Jf1EMYWziD26CaGzthTi", "kyoto", "none", "unapproved");
 
-insert into ADMINS(name, password)
+insert into admins(name, password)
 values("admin", "$2a$10$uhuqnvtjTayBhSjs7ezeB.2DG5GlIERAawzRoCROyTxWpzwKy7T.e");
