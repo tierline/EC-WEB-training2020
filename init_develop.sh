018f8df786b188080261a4c9773a97a117b971ab
tsukamoto <tsukamoto@tierline.com>
2021-01-14 14:25:53 +0900

    デプロイ用のシェルスクリプト作成

---
+++ init_develop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+read ip
+
+echo ${ip}
+
+path=`pwd`
+
+# echo '-----initialize-----'
+
+# gradle build
+
+# echo '-----build_success-----'
+
+# cd build/libs
+
+# echo '-----springboot-Run-----'
+
+# ./EC-WEB-training2020-0.0.1-SNAPSHOT.jar
+
+# #ec2内での実行↓
+# #java -jar /home/ec2-user/EC-WEB-training2020-0.0.1-SNAPSHOT.jar
+
+# echo '-----OK-----'
+
+# cd ../../
+
+# echo '-----finish-----'
